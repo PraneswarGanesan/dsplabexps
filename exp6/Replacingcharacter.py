988f380527f7a24cef8c290f3919d64e2f476aac
Praneswar G <118578571+PraneswarGanesan@users.noreply.github.com>
2025-02-12 19:02:20 +0530

    Create Replacingcharacter.py

---
+++ exp6/Replacingcharacter.py
@@ -0,0 +1,8 @@
+string = "HI! HI! HI! WELCOME TO PYTHON PROGRAMMING"
+print("original string:", string)
+
+replace1 = string.replace("HI", "HELLO")
+print("replaced string 1:", replace1)
+
+replace2 = replace1.replace("HELLO", "Hello", 3)
+print("replaced string 2:", replace2)
